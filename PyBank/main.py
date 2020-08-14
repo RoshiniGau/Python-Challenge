6be3ded592b57081d286408f332af02ae161577c
Roshini <roshini.12j@gmail.com>
2020-08-14 12:21:23 -0400

    Added comments and average change

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,24 +2,35 @@ import os
 import csv
 from pathlib import Path
 
+#Path for CSV file
 csvpath = os.path.join("Resources","PyBankbudget.csv")
-month_list=[]
-profit_loss_list=[]
-average_monthly_change_list=[]
-previous_month_amount=0
+
+#Empty list to iterate and store row's 
+month_list = []
+profit_loss_list = []
+average_monthly_change_list = []
+previous_month_amount = 0
+
+#Open csv file in read mode 
 with open(csvpath, 'r') as budget:
      
+	 # Store the contents in csvreader variable
     csvreader = csv.reader(budget, delimiter=',')
+	#  Skip header labels to iterate through variable
     next(csvreader,None)
-
+	
+# Iterate through the stored file contents
     for row in csvreader:
+	    # Append total months and total profit 
         month_list.append(str(row[0]))
         profit_loss_list.append(int(row[1]))
-    total_months=len(month_list)
-    net_profit_loss=0
-
+		
+	# Find total no of months, average change and total
+    total_months = len(month_list)
+    net_profit_loss = 0
+     
     for x in profit_loss_list:
-       net_profit_loss=net_profit_loss+x
+       net_profit_loss = net_profit_loss+x
 
 
     for x in range(len(profit_loss_list)):
@@ -33,8 +44,9 @@ with open(csvpath, 'r') as budget:
     length=len(average_monthly_change_list)
     total=sum(average_monthly_change_list)
     profit_loss_average=total/length
-    #print(profit_loss_average)
-
+    
+	
+# Greatest increase ans greatest decrease of profit's
 month_greatest_increase=''
 amount_greatest_increase=0
 month_greatest_decrease=''
@@ -46,18 +58,23 @@ for x in range(len(average_monthly_change_list)):
     elif average_monthly_change_list[x]<amount_greatest_decrease :
         amount_greatest_decrease=average_monthly_change_list[x]
         month_greatest_decrease=month_list[x+1]
+		
+# Output to terminal
 print("Financial Analysis")
 print("-------------------------------------------------------------------")
 print("Total Months :" +str(total_months))
 print("Total:"  +str(net_profit_loss))
+print("Average Change " +"$"  +str(profit_loss_average))
 print("The greatest increase in profits over the entire period was in " +month_greatest_increase  +" "+ "for $"  +str(amount_greatest_increase))
 print("The greatest decrease in profits over the entire period was in " +month_greatest_decrease  +" "+ "for $"  +str(amount_greatest_decrease))
 
+# Output to file
 file=open("output.txt","w")
 file.write("Financial Analysis" + "\n")
 file.write("-----------------------------------------------------------------" +"\n")
 file.write("Total Months :" +str(total_months) +"\n")
 file.write("Total:"  +str(net_profit_loss) +"\n")
+file.write("Average Change " +"$"  +str(profit_loss_average) +"\n")
 file.write("The greatest increase in profits over the entire period was in " +month_greatest_increase  +" "+ "for $"  +str(amount_greatest_increase) +"\n")
 file.write("The greatest decrease in profits over the entire period was in " +month_greatest_decrease  +" "+ "for $"  +str(amount_greatest_decrease) +"\n")
 file.close()
